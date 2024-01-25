cc29a8153580f9cf52b36df6ac016873100cf9b9
hakanicellioglu <95232280+hakanicellioglu@users.noreply.github.com>
2024-01-25 09:54:33 +0300

    single-or-double

---
+++ SingleOrDouble/SingleOrDouble/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleOrDouble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to this application.");
+            Console.WriteLine();
+            Console.Write("Enter the number you want to check: ");
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                SingleOrDouble(number);
+                Console.WriteLine();
+                Console.WriteLine("Programmer by hakanberkeicellioglu");
+                Console.WriteLine();
+                Console.WriteLine("[1] Return to menu");
+                Console.WriteLine("[2] Exit");
+                Console.WriteLine();
+                Console.WriteLine("Which action do you want to perform?");
+                try
+                {
+                    int process = Convert.ToInt32(Console.ReadLine());
+                    if(process == 1)
+                    {
+                        Console.Clear();
+                        Main(args);
+                    }
+                    else if(process == 2)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                    Console.WriteLine();
+                    Main(args);
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message.ToString());
+                Console.WriteLine();
+                Main(args);
+            }
+            Console.ReadLine();
+        }
+        public static void SingleOrDouble(int number)
+        {
+            if (number == 0)
+            {
+                Console.WriteLine(number + " is zero.");
+            }
+            else if (number % 2 == 0)
+            {
+                Console.WriteLine(number + " is double.");
+            }
+            else if (number % 2 == 1)
+            {
+                Console.WriteLine(number + " is single.");
+            }
+        }
+    }
+}
---
+++ SingleOrDouble/SingleOrDouble/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("SingleOrDouble")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SingleOrDouble")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("32768e61-1394-41cc-ac94-5fa411b6d9f7")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
